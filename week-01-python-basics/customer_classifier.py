f78087713f195f7cd91f9e4356bfb54da524d5d4
Sebas Hglz <sebashglz16@gmail.com>
2026-05-04 15:18:50 -0600

    Update customer classifier examples

--- week-01-python-basics/customer_classifier.py
+++ week-01-python-basics/customer_classifier.py
@@ -1,10 +1,13 @@
 def classify_customer(replied, requested_info, follow_up):
     if replied and requested_info and follow_up:
-        return "Hot client"
+        return "Hot lead"
     elif replied and requested_info:
-        return"Warm client"
+        return "Warm lead"
     else: 
-        return"Cold client"
+        return "Cold lead"
     
 customer_status = classify_customer(True, True, False)
 print(customer_status)
+print(classify_customer(True, True, True))
+print(classify_customer(True, False, False))
+print(classify_customer(False, False, False))
