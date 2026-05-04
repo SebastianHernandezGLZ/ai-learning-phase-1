7542232dc07592cc454ca239ab0617a09190bedc
Sebas Hglz <sebashglz16@gmail.com>
2026-05-04 15:57:23 -0600

    Practice variables and data types

--- week-01-python-basics/exercises.py
+++ week-01-python-basics/exercises.py
@@ -40,6 +40,13 @@ print("Días desde el contacto:", days_since_contact)
 
 print(f"El cliente {customer_name} tiene {age} años, está interesado en {career_interest} y llegó por {contact_channel}.")
 
+is_potential_customer = True
+lead_score_luis = 70
+
+print(f"{customer_name} tiene un lead score de {lead_score_luis}.")
+
+
+
 customer_name_2 = "Ana"
 age_2 = 19
 career_interest_2 = "Psicología"
@@ -48,10 +55,18 @@ replied_2 = False
 requested_info_2 = False
 follow_up_2 = False
 days_since_contact_2 = 7
+lead_score_ana = 20
+
+print("Nombre:", customer_name_2)
+print("Edad:", age_2)
+print("Carrera de interés:", career_interest_2)
+print("Medio de contacto:", contact_channel_2)
+print("Respondió:", replied_2)
+print("Pidió información:", requested_info_2)
+print("Tuvo seguimiento:", follow_up_2)
+print("Días desde el contacto:", days_since_contact_2)
 
 print(f"El cliente {customer_name_2} tiene {age_2} años, está interesado en {career_interest_2} y llegó por {contact_channel_2}.")
 
-is_potential_customer = True
-lead_score = 70
+print(f"{customer_name_2} tiene un lead score de {lead_score_ana}.")
 
-print(f"{customer_name} tiene un lead score de {lead_score}.")
