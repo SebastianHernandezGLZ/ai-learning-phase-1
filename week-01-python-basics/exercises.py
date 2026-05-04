f158f1202439d4b1ac2a5e1ffcbe0c6c0e565f63
Sebas Hglz <sebashglz16@gmail.com>
2026-05-04 15:51:56 -0600

    Practice variables and data types

--- week-01-python-basics/exercises.py
+++ week-01-python-basics/exercises.py
@@ -1,4 +1,6 @@
-customer_name = "Luis"
+# Día 1:
+
+'''customer_name = "Luis"
 age = 21
 career_interest = "Ingeniería en Inteligencia Artificial"
 contact_channel = "WhatsApp"
@@ -14,4 +16,42 @@ print("Respondió:", replied)
 print("Pidió información:", requested_info)
 print("Tuvo seguimiento:", follow_up)
 
-print(f"El cliente {customer_name} está interesado en {career_interest} y llegó por {contact_channel}.")
+print(f"El cliente {customer_name} está interesado en {career_interest} y llegó por {contact_channel}.")'''
+
+
+# Día 2: 
+customer_name = "Luis"
+age = 21
+career_interest = "Ingeniería en Inteligencia Artificial"
+contact_channel = "WhatsApp"
+replied = True
+requested_info = True
+follow_up = False
+days_since_contact = 3
+
+print("Nombre:", customer_name)
+print("Edad:", age)
+print("Carrera de interés:", career_interest)
+print("Medio de contacto:", contact_channel)
+print("Respondió:", replied)
+print("Pidió información:", requested_info)
+print("Tuvo seguimiento:", follow_up)
+print("Días desde el contacto:", days_since_contact)
+
+print(f"El cliente {customer_name} tiene {age} años, está interesado en {career_interest} y llegó por {contact_channel}.")
+
+customer_name_2 = "Ana"
+age_2 = 19
+career_interest_2 = "Psicología"
+contact_channel_2 = "Instagram"
+replied_2 = False
+requested_info_2 = False
+follow_up_2 = False
+days_since_contact_2 = 7
+
+print(f"El cliente {customer_name_2} tiene {age_2} años, está interesado en {career_interest_2} y llegó por {contact_channel_2}.")
+
+is_potential_customer = True
+lead_score = 70
+
+print(f"{customer_name} tiene un lead score de {lead_score}.")
